11256dd881c04f58e27eda9327525a8ae4cf47d8
Aabhishek29 <starkabhishek29@gmail.com>
2022-07-20 13:19:31 +0530

    First Commit

---
+++ LoginWindow.py
@@ -0,0 +1,95 @@
+from threading import Thread
+import threading
+import tkinter
+from tkinter import *
+import os
+import Colors
+import re
+from Utils import Utility
+
+
+class LoginWindow():
+    def __init__(self , root , util) -> None:
+        self.root = root
+        self.util = util
+        root.title("Login - VoiceBasedMailSystem - "+os.getcwd())
+        root.geometry("1080x720")
+        root.maxsize(1080,720)
+        root.minsize(1080,720)
+        self.e_state = 0
+        self.p_state = 0
+
+    def mainCanvas(self): 
+        self.canvas1 = Canvas( self.root,background=Colors.color['themedark'])
+        self.canvas1.pack(fill = "both", expand = True)
+        self.canvas1.background =PhotoImage(file = "./assets/login_canvas_bg.png")
+        self.canvas1.create_image( 0, 0, image = self.canvas1.background,anchor = "nw")
+
+
+    def loginView(self):
+    
+        self.loginCan = self.canvas1
+        Label(self.loginCan, text="Welcome !", fg=Colors.color['white'],background=Colors.color['themedark'] , font=('Times',40,'bold')).place(x=300,y=150)
+
+        Label(self.loginCan, text="Email", fg=Colors.color['white'], background=Colors.color['themedark'],font=('Times',18,'bold')).place(x=350,y=280)
+        framenamefield = Frame(self.loginCan)
+        framenamefield.place(x = 350, y=320)
+        self.email_input = Text(framenamefield,bd=0, height=1, width=40,font=('Times',16),bg=Colors.color['white'])
+        self.email_input.pack(padx=5,pady=5)
+
+        Label(self.loginCan, text="Password", fg=Colors.color['white'],background=Colors.color['themedark'], font=('Times',18,'bold')).place(x=350,y=390)
+        framepassfield = Frame(self.loginCan)
+        framepassfield.place( x = 350, y=430)
+        self.password_input = Text(framepassfield,bd=0, height=1, width=40,fg=Colors.color['black'],font=('Times',16),bg=Colors.color['white'])
+        self.password_input.pack(padx=5,pady=5)
+
+        framebtn = Frame(self.loginCan)
+        framebtn.place(x= 480, y=550 )
+        self.btn = Button(framebtn, text="Login", bg=Colors.color['primaryblue'], fg=Colors.color['white'],font=('Times',20),command=self.loginUser)
+        self.btn.pack(ipadx=35,ipady=5,fill='both')
+
+    def loginUser(self):
+        userId = self.email_input.get(1.0,"end-1c")
+        pswd = self.password_input.get(1.0,"end-1c")
+        if not (self.checkEmail(userId) and self.checkpassword(pswd)):
+            print("Bad Credentials Not Passed by ")
+        print(f"User id = {userId}")
+        print(f"Password = {pswd}")
+
+        if not self.authenticate(userId,pswd):
+            Label(self.canvas1,text="wrong email/password",bg=Colors.color['themedark'],fg=Colors.color['red'],font=('Times',18)).place(x=440,y=500)
+        print("Id and Password Authenticated")
+
+    def checkEmail(self,mail) -> bool:
+        regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+        if(not re.fullmatch(regex, mail)):
+            self.p_state+=1
+            if self.p_state == 1:
+                Label(self.canvas1,text="please re-enter your email address",bg=Colors.color['themedark'],fg=Colors.color['red']).place(x=350,y=360)
+            return False
+        return True
+
+    def checkpassword(self,pswd) -> bool:
+        if not len(pswd) > 2:
+            self.p_state+=1
+            if self.p_state == 1:
+                Label(self.canvas1,text="please re-enter your email address",bg=Colors.color['themedark'],fg=Colors.color['red']).place(x=350,y=470)
+            return False
+        return True
+
+    def authenticate(self,mailId,pswd) -> bool:
+        return False
+
+    def startLoginWindow(self):
+        self.mainCanvas()
+        self.loginView()
+
+
+if __name__ == '__main__':
+    root = tkinter.Tk()
+    util = Utility()
+    win = LoginWindow(root,util)
+
+    win.startLoginWindow()
+
+    root.mainloop()
